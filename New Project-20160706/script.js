cd1ff7403fc78ef03bb8d8ba24ef933927418ee2
yang1500313979 <1500313979@qq.com>
2016-07-06 16:59:55 +0800

    Created by Coding Ground Client

---
+++ New Project-20160706/script.js
@@ -0,0 +1,10 @@
+function sayHello()
+{
+//   alert("Hello, Coding Ground!");
+var URL1 = "www.baidu.com"
+var add = '{"data":{"scheme":'+URL1+'}}';
+// conlog.log(add);
+alert(add);
+}
+
+sayHello();
